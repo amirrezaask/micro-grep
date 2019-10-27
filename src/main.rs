0ece9cd9edb6884696f1930528c925cea8307086
amirrezaask <raskarpour@gmail.com>
2019-10-27 17:01:18 +0330

    reading cli input

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    if args.len() < 3 {
         panic!("need at least 2 arguments")
     }
+    let name = &args[1];
+    let query = &args[2];
 
+    println!("{}", name);
+    println!("{}", query);
 }
